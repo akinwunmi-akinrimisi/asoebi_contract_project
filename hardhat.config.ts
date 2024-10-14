cd1c40e5ef330eb5c4e9f0f27d64a0aeefb6e94c
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-10-14 23:27:11 +0100

    test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,66 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
+
+const {
+  SEPOLIA_RPC_URL,
+  ACCOUNT_PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+  ALCHEMY_API_KEY_URL,
+} = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.27',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+  solidity: '0.8.24',
+  networks: {
+    hardhat: {
+      forking: {
+        url: ALCHEMY_API_KEY_URL!,
       },
     },
+    sepolia: {
+      url: SEPOLIA_RPC_URL || '',
+      accounts: ACCOUNT_PRIVATE_KEY !== undefined ? [ACCOUNT_PRIVATE_KEY] : [],
+    },
+    // for testnet
+    //
+    'lisk-sepolia': {
+      url: process.env.LISK_RPC_URL!,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+      gasPrice: 1000000000,
+    },
+    scrollSepolia: {
+      url: process.env.SCROLL_RPC_URL!,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+      // gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+    apiKey: {
+      'lisk-sepolia': '123',
+      scrollSepolia: process.env.SCROLL_API_KEY!,
+    },
+    customChains: [
+      {
+        network: 'lisk-sepolia',
+        chainId: 4202,
+        urls: {
+          apiURL: 'https://sepolia-blockscout.lisk.com/api',
+          browserURL: 'https://sepolia-blockscout.lisk.com/',
+        },
+      },
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia.scrollscan.com/',
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
   },
 };
 
